ae8af3e31b7c6c37edc4b3752839e80750d63d78
Marco Giugliani <marco.giugliani@studio.unibo.it>
2024-02-03 13:05:12 +0100

    Updated 'Ingredient' interface with the insertion of the methods 'supply' and 'reduce'

--- src/main/java/it/unibo/model/api/Ingredient.java
+++ src/main/java/it/unibo/model/api/Ingredient.java
@@ -1,5 +1,14 @@
 package it.unibo.model.api;
 
 public interface Ingredient {
-    
+    /**
+     * Increase the quantity of the ingredient to the maximum available
+     */
+    void supply();
+
+    /**
+     * Decrease the quantity of the ingredient when it is put on a pizza
+     * NOTE: the quantity is different for each ingredient
+     */
+    void reduce();
 }
