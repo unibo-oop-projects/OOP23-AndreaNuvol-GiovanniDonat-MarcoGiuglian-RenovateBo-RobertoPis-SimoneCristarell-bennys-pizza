29ff015dd231dafe5d3c67cf51b1b6047bad19af
Marco Giugliani <marco.giugliani@studio.unibo.it>
2024-02-07 17:07:45 +0100

    Modified a comment in the 'Ingredient' interface

--- src/main/java/it/unibo/model/api/Ingredient.java
+++ src/main/java/it/unibo/model/api/Ingredient.java
@@ -5,12 +5,12 @@ package it.unibo.model.api;
  */
 public interface Ingredient {
     /**
-     * Increase the quantity of the ingredient to the maximum available.
+     * It increases the quantity of the ingredient to the maximum available.
      */
     void supply();
 
     /**
-     * Decrease the quantity of the ingredient when it is put on a pizza.
+     * It decreases the quantity of the ingredient when it is put on a pizza.
      * NOTE: the quantity is different for each ingredient.
      */
     void reduce();
