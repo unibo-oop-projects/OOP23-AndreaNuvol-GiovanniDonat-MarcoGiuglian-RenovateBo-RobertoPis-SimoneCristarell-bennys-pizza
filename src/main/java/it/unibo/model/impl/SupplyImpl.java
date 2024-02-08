97a9563e31a7ea1750ee85fc04fbeb67a58f8592
Simone Cristarelli <s.cristarelli@gmail.com>
2024-02-08 18:03:04 +0100

    Add 'SupplyImpl' to 'impl' folder and start to write its methods

---
+++ src/main/java/it/unibo/model/impl/SupplyImpl.java
@@ -0,0 +1,17 @@
+package it.unibo.model.impl;
+
+import it.unibo.model.api.Supply;
+import it.unibo.model.api.Ingredient;
+
+public class SupplyImpl implements Supply {
+
+    @Override
+    public final void supply(Ingredient Ingredient) {
+        ingredient.supply;
+
+    }
+
+    private static final void payment() {
+        
+    }
+}
