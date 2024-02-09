6db8c82c9d7fcb6f61e8c46a1303e63a06dae118
Marco Giugliani <marco.giugliani@studio.unibo.it>
2024-02-09 15:51:09 +0100

    Added the 'PizzaFactoryImpl' class in 'impl' folder

---
+++ src/main/java/it/unibo/model/impl/PizzaFactoryImpl.java
@@ -0,0 +1,27 @@
+package it.unibo.model.impl;
+
+import java.util.Collection;
+
+import it.unibo.model.api.Ingredient;
+import it.unibo.model.api.PizzaFactory;
+
+/**
+ * Implementation of the 'PizzaFactory' interface
+ */
+public class PizzaFactoryImpl implements PizzaFactory{
+
+    @Override
+    public void addIngredient(Ingredient ingredientToAdd) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addIngredient'");
+    }
+
+    @Override
+    public boolean equals(Collection<? extends Ingredient> ingredients) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'equals'");
+    }
+
+
+
+}
