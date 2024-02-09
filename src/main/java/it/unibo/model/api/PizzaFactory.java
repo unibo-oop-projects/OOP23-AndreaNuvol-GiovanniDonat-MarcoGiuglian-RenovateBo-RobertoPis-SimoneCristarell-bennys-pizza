926335109d7db9ffe1b04166a49523044855c992
Marco Giugliani <marco.giugliani@studio.unibo.it>
2024-02-09 15:40:22 +0100

    Added the 'equals' method in 'PizzaFactory' interface

--- src/main/java/it/unibo/model/api/PizzaFactory.java
+++ src/main/java/it/unibo/model/api/PizzaFactory.java
@@ -1,5 +1,7 @@
 package it.unibo.model.api;
 
+import java.util.Collection;
+
 /**
  * Interface of a pizza factory
  */
@@ -10,5 +12,12 @@ public interface PizzaFactory {
      * @param ingredientToAdd the ingredient to add
      */
     void addIngredient(Ingredient ingredientToAdd);
+
+    /**
+     * @param ingredients the ingredients of the requested pizza
+     * @return true if the ingredients of the prepared pizza are the same of the requested one's (i.e. the pizzas 
+     * are equal), false otherwise
+     */
+    boolean equals(Collection<? extends Ingredient> ingredients);
 
 }
