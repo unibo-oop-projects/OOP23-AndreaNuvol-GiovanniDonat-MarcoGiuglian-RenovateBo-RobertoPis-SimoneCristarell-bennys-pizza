d1ede292ff9eb7f2a57807e1398d1508ec9b436c
Marco Giugliani <marco.giugliani@studio.unibo.it>
2024-02-11 22:27:02 +0100

    Improved the inizialization of each ingredient in 'PreparationZoneImpl' class' constructor using the reflection

--- src/main/java/it/unibo/model/impl/PreparationZoneImpl.java
+++ src/main/java/it/unibo/model/impl/PreparationZoneImpl.java
@@ -1,12 +1,15 @@
 package it.unibo.model.impl;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import it.unibo.model.api.*;
-import it.unibo.model.impl.IngredientsImpl.*;
+// import it.unibo.model.impl.IngredientsImpl.*;
 public class PreparationZoneImpl implements PreparationZone {
 
     private static final int MIN_PIZZE_TO_PREPARE = 1;
@@ -15,31 +18,23 @@ public class PreparationZoneImpl implements PreparationZone {
     private Optional<PizzaFactory> pizza2 = Optional.empty();
     private final Oven oven = new OvenImpl();
     private final Map<Ingredient, Integer> ingredientsQuantities = new HashMap<>();
+    
 
-    public PreparationZoneImpl(final int numPizzeToPrepare) {
+    public PreparationZoneImpl(final int numPizzeToPrepare) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
         if (numPizzeToPrepare < MIN_PIZZE_TO_PREPARE || numPizzeToPrepare > MAX_PIZZE_TO_PREPARE) {
             throw new IllegalArgumentException("The number of pizzas to prepare can be only 1 or 2.");
         } else if (numPizzeToPrepare == 2) {
             this.pizza2 = Optional.of(new PizzaFactoryImpl());
         }
-        ingredientsQuantities.put(new Anchovy(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Artichoke(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new CherryTomatoe(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Dough(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Fontina(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new FrenchFry(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Gorgonzola(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Ham(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Mozzarella(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Mushroom(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Olive(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Onion(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Parmesan(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Salami(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Sausage(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new TomatoSauce(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Tuna(), IngredientImpl.MAX_QUANTITY);
-        ingredientsQuantities.put(new Wurstel(), IngredientImpl.MAX_QUANTITY);
+
+        List<String> classes = new ArrayList<>(List.of("Anchovy", "Artichoke", "CherryTomatoe", "Dough", 
+            "Fontina", "FrenchFry", "Gorgonzola", "Ham", "Mozzarella", "Mushroom", "Olive", "Onion", "Parmesan", 
+            "Salami", "Sausage", "TomatoSauce", "Tuna", "Wurstel"));
+
+        for (final var cl: classes) {
+            final var clazz = Class.forName(this.getClass().getPackageName() + ".IngredientsImpl." + cl);
+            this.ingredientsQuantities.put((Ingredient)clazz.getConstructor().newInstance(), IngredientImpl.MAX_QUANTITY);
+        }
     }
 
     @Override
