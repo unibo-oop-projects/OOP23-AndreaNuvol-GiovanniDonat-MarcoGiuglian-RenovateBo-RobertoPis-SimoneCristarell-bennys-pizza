b8b191b8617fc7db8a9ecb4df43ecfed4bf79b34
Marco Giugliani <marco.giugliani@studio.unibo.it>
2024-02-12 16:45:02 +0100

    Added 'CleanerImpl' class in 'impl' folder

---
+++ src/main/java/it/unibo/model/impl/CleanerImpl.java
@@ -0,0 +1,13 @@
+package it.unibo.model.impl;
+
+import it.unibo.model.api.Cleaner;
+
+public class CleanerImpl implements Cleaner {
+
+    @Override
+    public void clean() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clean'");
+    }
+    
+}
