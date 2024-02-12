aa7ad6dea93e68e063a8a9ebbd3b1f19d02dd5ff
Andrea Nuvoli <andrea.nuvoli@studio.unibo.it>
2024-02-12 21:41:32 +0100

    Deleted Pizza.java and created an inner static class Pizza inside MenuImpl

--- src/main/java/it/unibo/model/impl/MenuImpl.java
+++ src/main/java/it/unibo/model/impl/MenuImpl.java
@@ -49,4 +49,30 @@ public class MenuImpl implements Menu {
     public void update() {
         
     }
+
+    static class Pizza {
+    
+        private final String name;
+        private final float cost;
+        private List<String> ingredients;
+    
+        public Pizza(String name, float cost, List<String> ingredients) {
+            this.name = name;
+            this.cost = cost;
+            this.ingredients = ingredients;
+        }
+    
+        public String getName() {
+            return this.name;
+        }
+    
+        public List<String> getIngredients() {
+            return this.ingredients;
+        }
+    
+        public float getCost() {
+            return this.cost;
+        }
+        
+    }
 }
