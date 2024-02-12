be61131b73689c9a5202d5409c36263ea9882b47
Andrea Nuvoli <andrea.nuvoli@studio.unibo.it>
2024-02-12 09:22:40 +0100

    Changed dependency from GSON to Jackson

--- src/main/java/it/unibo/model/impl/Pizza.java
+++ src/main/java/it/unibo/model/impl/Pizza.java
@@ -4,9 +4,15 @@ import java.util.*;
 
 public class Pizza {
     
-    private String name;
-    private float cost;
+    private final String name;
+    private final float cost;
     private List<String> ingredients;
+
+    public Pizza(String name, float cost, List<String> ingredients) {
+        this.name = name;
+        this.cost = cost;
+        this.ingredients = ingredients;
+    }
 
     public String getName() {
         return this.name;
