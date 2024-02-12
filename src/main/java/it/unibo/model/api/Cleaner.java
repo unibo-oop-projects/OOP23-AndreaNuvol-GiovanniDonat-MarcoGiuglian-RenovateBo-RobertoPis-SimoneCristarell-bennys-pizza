7179869e3f8cafff97feedb0d762f3b38c25b733
Marco Giugliani <marco.giugliani@studio.unibo.it>
2024-02-12 16:38:40 +0100

    Added 'Cleaner' interface in 'api' folder

---
+++ src/main/java/it/unibo/model/api/Cleaner.java
@@ -0,0 +1,13 @@
+package it.unibo.model.api;
+
+/**
+ * Interface of a preparation zone cleaner
+ */
+public interface Cleaner {
+    
+    /**
+     * It cleans the preparation zone, if it is dirty
+     */
+    void clean();
+
+}
