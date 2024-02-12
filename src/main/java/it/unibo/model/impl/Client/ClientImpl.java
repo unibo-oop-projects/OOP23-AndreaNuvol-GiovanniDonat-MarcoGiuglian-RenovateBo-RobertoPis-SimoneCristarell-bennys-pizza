8755ff67faffb5ab4ec28e9ea432098bd512b022
Roberto Pisu <roberto.pisu03@gmail.com>
2024-02-12 12:15:01 +0100

    Added menu in class ClientImpl

--- src/main/java/it/unibo/model/impl/Client/ClientImpl.java
+++ src/main/java/it/unibo/model/impl/Client/ClientImpl.java
@@ -1,6 +1,8 @@
 package it.unibo.model.impl.Client;
 
 import it.unibo.model.api.Client;
+import it.unibo.model.api.Menu;
+import it.unibo.model.impl.MenuImpl;
 import it.unibo.model.impl.Management.AbstractManager;
 import it.unibo.model.impl.Management.AdderManager; // The client can only add money to the balance
 
@@ -9,12 +11,22 @@ public class ClientImpl implements Client{
     @Override
     public void order() {
         // creare ordine prendendo 2 nomi di pizze in modo randomico dal menu
+        // prendo il menu
+        Menu menu = new MenuImpl();
+        
+        
     }
 
     @Override
     public void pay() {
         // se la pizza va bene paga il prezzo della pizza
         AbstractManager manager = new AdderManager();
+    }
+
+    public static void main(String[] args) {
+        MenuImpl menu = new MenuImpl();
+        // menu.generateMenu();
+        menu.show();
     }
     
 }
