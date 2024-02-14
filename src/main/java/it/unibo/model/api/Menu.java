be00ad2972308b64bce1e8828bc79d6db77f2957
Andrea Nuvoli <andrea.nuvoli@studio.unibo.it>
2024-02-14 15:05:36 +0100

    Made some changes on TimeImpl and MenuImpl

--- src/main/java/it/unibo/model/api/Menu.java
+++ src/main/java/it/unibo/model/api/Menu.java
@@ -3,14 +3,9 @@ package it.unibo.model.api;
 public interface Menu {
     
     /**
-     * Mostra il menu leggendolo direttamente dal file menu.json.
-     * Deve essere usato solamente una volta, all'inizio.
+     * Read the menu from the file menu.json.
+     * It only needs to be used once, at the beginning.
      */
     void show();
-
-    /**
-     * Permette di aggiornare il menu in caso di aggiunta di una pizza.
-     */
-    void update();
 
 }
--- src/main/java/it/unibo/model/impl/Time/TimeImp.java
+++ src/main/java/it/unibo/model/impl/Time/TimeImp.java
@@ -6,7 +6,7 @@ import it.unibo.model.impl.Management.AbstractManager;
 
 public class TimeImp {
 
-    final static int TIME_FOR_15_MINUTES = 15000; // millisecondi per far passare 15 minuti nel gioco
+    final static int TIME_FOR_15_MINUTES = 15_000;
 
     final static int STARTING_HOUR = 10;
     final static int STARTING_MIN = 0;
@@ -14,7 +14,7 @@ public class TimeImp {
     final static int ENDING_HOUR = 22;
     final static int ENDING_MIN = 30;
     
-    public int workingDays = 1;
+    private int workingDays = 1;
     private int hour;
     private int min;
     private Timer timer;
@@ -37,10 +37,7 @@ public class TimeImp {
                     min += 15;
                 }
 
-                // System.out.println("Orario: Ora : [ " + getHour() + " ] \t Minuti : [ " + getMin() + " ]\n");
-
                 if(isEndOfDay()) {
-                    // System.out.println("FINE GIORNATA!\nOrario: Ora : [ " + getHour() + " ] \t Minuti : [ " + getMin() + " ]\n");
                     timer.cancel();
                     if(AbstractManager.levelPassed()) {
                         workingDays++;
@@ -50,7 +47,7 @@ public class TimeImp {
             
         };
 
-        timer.scheduleAtFixedRate(task, 0, TIME_FOR_15_MINUTES); // test with 1000ms
+        timer.scheduleAtFixedRate(task, 0, TIME_FOR_15_MINUTES);
     }
 
     public void newDay() {
@@ -60,5 +57,9 @@ public class TimeImp {
 
     private boolean isEndOfDay() {
         return this.hour == ENDING_HOUR && this.min == ENDING_MIN ? true : false;
+    }
+
+    public int getWorkingDay() {
+        return this.workingDays;
     }
 }
