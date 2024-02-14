0518b1b56eade71eab49b850d3b0ff28df865aaf
Roberto Pisu <roberto.pisu03@gmail.com>
2024-02-14 16:56:28 +0100

    modified AdderManager and SubtractorManager to work with balanceDay

--- src/main/java/it/unibo/model/impl/Management/AdderManager.java
+++ src/main/java/it/unibo/model/impl/Management/AdderManager.java
@@ -5,7 +5,7 @@ public class AdderManager extends AbstractManager{
     @Override
     public void updateBalance(final double amount) {
         if(isDepositAllowed(amount)){
-            balance += amount;
+            balanceDay += amount;
         }else{
             throw new IllegalArgumentException("Can't add negative or null value to balance.");
         }
