ba94d8733c46a0d72483d1c4670b0d6ef8f2880e
Marco Giugliani <marco.giugliani@studio.unibo.it>
2024-02-15 11:45:27 +0100

    Added the controller methods named 'getPreparationZone' and 'addIngredient'

--- src/main/java/it/unibo/controller/api/Controller.java
+++ src/main/java/it/unibo/controller/api/Controller.java
@@ -1,5 +1,24 @@
 package it.unibo.controller.api;
 
+import it.unibo.model.api.PreparationZone;
+
+/**
+ * Controller interface
+ */
 public interface Controller {
-    void test();
+    
+    /**
+     * It adds an ingredient to the pizza you are preparing
+     * @param ingredientStringToAdd the string of the ingredient to add
+     * @param isPizza1 true if you are preparing pizza n. 1, false otherwise
+     * @param isASupply true if you are supplying an ingredient, false otherwise
+     */
+    void addIngredient(String ingredientStringToAdd, boolean isPizza1, boolean isASupply);
+
+    /**
+     * It gets the preparation zone where you are working
+     * @return the preparation zone where you are working
+     */
+    PreparationZone getPreparationZone();
+
 }
--- src/main/java/it/unibo/controller/impl/ControllerImpl.java
+++ src/main/java/it/unibo/controller/impl/ControllerImpl.java
@@ -1,7 +1,32 @@
 package it.unibo.controller.impl;
 
-import it.unibo.controller.api.Controller;
+import java.lang.reflect.InvocationTargetException;
 
-public class ControllerImpl implements Controller{
-    
+import it.unibo.controller.api.Controller;
+import it.unibo.model.api.PreparationZone;
+import it.unibo.model.impl.PreparationZoneImpl;
+import it.unibo.model.impl.Management.SubtractorManager;
+
+/**
+ * Implementation of the controller interface
+ */
+public class ControllerImpl implements Controller {
+
+    private final SubtractorManager subtractor = new SubtractorManager();
+    private final PreparationZone preparationZone;
+
+    public ControllerImpl() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+        this.preparationZone = new PreparationZoneImpl(this.subtractor);
+    }
+
+    @Override
+    public PreparationZone getPreparationZone() {
+        return this.preparationZone;
+    }
+
+    @Override
+    public void addIngredient(final String ingredientStringToAdd, final boolean isPizza1, final boolean isASupply) {
+        this.preparationZone.actionsOnIngredients(ingredientStringToAdd, isPizza1, isASupply);
+    }
+
 }
