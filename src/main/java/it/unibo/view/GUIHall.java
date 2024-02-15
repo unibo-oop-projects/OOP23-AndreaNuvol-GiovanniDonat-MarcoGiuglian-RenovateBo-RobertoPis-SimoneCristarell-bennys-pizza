645c19c8dc3d2c9766a10aee6e3197bca9637a21
Roberto Pisu <roberto.pisu03@gmail.com>
2024-02-15 18:41:25 +0100

    added method displayLabelsBalance that display labels with balances

--- src/main/java/it/unibo/view/GUIHall.java
+++ src/main/java/it/unibo/view/GUIHall.java
@@ -8,6 +8,8 @@ import javax.swing.*;
 public class GUIHall {
 
     final static String SEP = File.separator;
+    private static final String BALANCE_TOT = "Balance tot: ";
+    private static final String BALANCE_DAY = "Balance day: ";
     private static final String PATH_TO_THE_ROOT = FileSystems.getDefault().getPath(new String()).toAbsolutePath().toString();
     private static final String FILE_PATH = SEP         +
                                             "src"       + SEP +
@@ -25,10 +27,31 @@ public class GUIHall {
             JFrame frame = new JFrame("Image Background Example");
             Image backgroundImage = Toolkit.getDefaultToolkit().getImage(PATH_TO_THE_ROOT + FILE_PATH);
             ImagePanel imagePanel = new ImagePanel(backgroundImage);
-            frame.getContentPane().add(imagePanel);
+            frame.getContentPane().setLayout(new BorderLayout()); // Imposta il layout manager del frame
+            frame.getContentPane().add(imagePanel, BorderLayout.CENTER);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(width, height);
             frame.setVisible(true);
+
+
+            displayLabelsBalance(imagePanel);
+            
         });
-    }    
+    }
+
+    public static void main(String[] args) {
+        new GUIHall();
+    }
+
+    private void displayLabelsBalance(final ImagePanel imagePanel){
+        JLabel labelTotalBalance = new JLabel(BALANCE_TOT);
+        JLabel labelDayBalance = new JLabel(BALANCE_DAY);
+        Font fontLabelBalanceTot = labelTotalBalance.getFont().deriveFont(Font.BOLD, 20);
+        Font fontLabelBalanceDay = labelTotalBalance.getFont().deriveFont(Font.BOLD, 25);
+        labelTotalBalance.setFont(fontLabelBalanceTot);
+        labelDayBalance.setFont(fontLabelBalanceDay);
+        imagePanel.setLayout(new BoxLayout(imagePanel, BoxLayout.Y_AXIS));
+        imagePanel.add(labelTotalBalance);
+        imagePanel.add(labelDayBalance);
+    }
 }
