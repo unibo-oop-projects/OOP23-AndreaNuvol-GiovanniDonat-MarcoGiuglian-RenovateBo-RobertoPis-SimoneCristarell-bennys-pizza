fbdc588102726f368cfa2715126984fecde0037c
Roberto Pisu <roberto.pisu03@gmail.com>
2024-02-16 14:37:01 +0100

    modified the method that display labels that contains balances in a way that they are visible

--- src/main/java/it/unibo/view/GUIHall.java
+++ src/main/java/it/unibo/view/GUIHall.java
@@ -33,59 +33,56 @@ public class GUIHall {
             JFrame background = new JFrame("BENNY'S PIZZA");
             Image backgroundImage = Toolkit.getDefaultToolkit().getImage(PATH_TO_THE_ROOT + FILE_PATH_BACKGROUND);
             ImagePanel imagePanel = new ImagePanel(backgroundImage);
-            
-            frontView(imagePanel, background);
-            displayLabelsBalance(imagePanel);
+            background.getContentPane().add(imagePanel);
+            background.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            background.setSize(width, height);
+    
+            displayMenu(imagePanel, background);
+            displayBalanceLabels(imagePanel, background);
             displayClient(imagePanel);
-            
         });
     }
 
-    private void frontView(ImagePanel imagePanel, JFrame background) {
-        
+    private void displayMenu(final ImagePanel imagePanel, final JFrame background) {
         imagePanel.setLayout(new BorderLayout());
-
-        background.getContentPane().add(imagePanel);
-        background.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        background.setSize(width, height);
-
         JPanel menuPanel = new JPanel(new BorderLayout());
-        setMenuPanelAttributes(menuPanel);
-
+        setPanelAttributes(menuPanel);
         JButton menuButton = new JButton("Menu");
         setMenuButtonAttributes(menuButton);
-        
         menuPanel.add(menuButton, BorderLayout.EAST);
         imagePanel.add(menuPanel, BorderLayout.SOUTH);
-        
         background.setVisible(true); // da mettere false quando si passa alla schermata della cucina 
     }
 
-    private void setMenuPanelAttributes(JPanel menuPanel) {
+    private void displayBalanceLabels(final ImagePanel imagePanel, final JFrame background){
+        imagePanel.setLayout(new BorderLayout());
+        JPanel balanceLabelsPanel = new JPanel();
+        balanceLabelsPanel.setLayout(new BoxLayout(balanceLabelsPanel, BoxLayout.Y_AXIS));
+        setPanelAttributes(balanceLabelsPanel);
+        JLabel balanceTotLabel = new JLabel(BALANCE_TOT);
+        JLabel balanceDayLabel = new JLabel(BALANCE_DAY);
+        Font fontLabelBalanceTot = balanceTotLabel.getFont().deriveFont(Font.BOLD, 20);
+        Font fontLabelBalanceDay = balanceDayLabel.getFont().deriveFont(Font.BOLD, 25);
+        balanceTotLabel.setFont(fontLabelBalanceTot);
+        balanceDayLabel.setFont(fontLabelBalanceDay);
+        balanceLabelsPanel.add(balanceTotLabel);
+        balanceLabelsPanel.add(balanceDayLabel);
+        imagePanel.add(balanceLabelsPanel, BorderLayout.NORTH);
+        background.setVisible(true);
+    }
+
+    private void setPanelAttributes(final JPanel menuPanel) {
         menuPanel.setOpaque(false);
         menuPanel.setBackground(new Color(0, 0, 0, 0));
         menuPanel.setBorder(new EmptyBorder(10, 10, 50, 10));
     }
 
-    private void setMenuButtonAttributes(JButton menuButton) {
+    private void setMenuButtonAttributes(final JButton menuButton) {
         menuButton.setBackground(new Color(Integer.parseInt("FF7F50", 16)));
         menuButton.setBorderPainted(false);
         menuButton.setFocusPainted(false);
         menuButton.setFont(new Font("Arial", Font.BOLD, 30));
         menuButton.setSize(MENU_WIDTH, MENU_WIDTH);
-    }
-
-    private void displayLabelsBalance(final ImagePanel imagePanel){
-
-        JLabel labelTotalBalance = new JLabel(BALANCE_TOT);
-        JLabel labelDayBalance = new JLabel(BALANCE_DAY);
-        Font fontLabelBalanceTot = labelTotalBalance.getFont().deriveFont(Font.BOLD, 20);
-        Font fontLabelBalanceDay = labelTotalBalance.getFont().deriveFont(Font.BOLD, 25);
-        labelTotalBalance.setFont(fontLabelBalanceTot);
-        labelDayBalance.setFont(fontLabelBalanceDay);
-        imagePanel.setLayout(new BoxLayout(imagePanel, BoxLayout.Y_AXIS));
-        imagePanel.add(labelTotalBalance);
-        imagePanel.add(labelDayBalance);
     }
 
     // Show a client different from the last showed, it return the index 
