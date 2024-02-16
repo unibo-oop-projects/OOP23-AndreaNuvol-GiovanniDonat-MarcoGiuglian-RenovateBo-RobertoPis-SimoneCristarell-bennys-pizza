98d54b88be0b0fb985c7c39920afc4623846ffac
Marco Giugliani <marco.giugliani@studio.unibo.it>
2024-02-16 18:59:30 +0100

    Added the garbage bin resizable button and added some internal methods

--- src/main/java/it/unibo/view/GUIKitchen.java
+++ src/main/java/it/unibo/view/GUIKitchen.java
@@ -1,17 +1,23 @@
 package it.unibo.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.FileSystems;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import it.unibo.controller.api.Controller;
 import it.unibo.controller.impl.ControllerImpl;
@@ -29,22 +35,55 @@ public class GUIKitchen {
     private final JFrame frame = new JFrame("KITCHEN");
     
     public GUIKitchen(final Controller controller) {
+        frame.setSize(screenWidth, screenHeight);
         final Image background = Toolkit.getDefaultToolkit().getImage(PATH_TO_THE_ROOT + PATH_TO_RESOURCES + 
             "Preparation_Zone.png");
         ImagePanel imagePanel = new ImagePanel(background);
         frame.getContentPane().setLayout(new BorderLayout());
         frame.getContentPane().add(imagePanel, BorderLayout.CENTER);
         imagePanel.setLayout(new BorderLayout());
-        final JPanel highPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, (int)(screenWidth / 35), 10));
+        
+        displayInfoLabels(imagePanel);
 
+        final JPanel lowPanel = new JPanel(new BorderLayout());
+        lowPanel.setOpaque(false);
+        final Image garbageBin = Toolkit.getDefaultToolkit().getImage(PATH_TO_THE_ROOT + PATH_TO_RESOURCES + 
+            "KitchenComponentsImages" + SEP + "GarbageBin.png");
+        final JButton btnGarbageBin = new JButton();
+        btnGarbageBin.setBackground(new Color(195, 195, 195, 255));
+        btnGarbageBin.setBorderPainted(false);
+        lowPanel.add(btnGarbageBin, BorderLayout.EAST);
+        displayGarbageBinButton(btnGarbageBin, garbageBin, frame.getWidth(), frame.getHeight(), lowPanel);
+        imagePanel.add(lowPanel, BorderLayout.SOUTH);
+
+        frame.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(final ComponentEvent e) {
+                int width = frame.getContentPane().getWidth();
+                int height = frame.getContentPane().getHeight();
+                displayInfoLabels(imagePanel);
+                displayGarbageBinButton(btnGarbageBin, garbageBin, width, height, lowPanel);
+            }
+        });
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void displayInfoLabels(final ImagePanel imagePanel) {
+        final JPanel highPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, (int)(frame.getWidth() / 35), 10));
         final JLabel lblDay = new JLabel("Day: 16/02/2024");
         highPanel.add(lblDay);
         highPanel.add(new JLabel("18:15"));
         highPanel.add(new JLabel("Balance: 50€"));
         imagePanel.add(highPanel, BorderLayout.NORTH);
+    }
 
-        frame.setSize(screenWidth, screenHeight);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private void displayGarbageBinButton(final JButton bin, final Image garbageBin, final int width, final int height, final JPanel lowPanel) {
+        bin.setSize(new Dimension((int)(width * 0.08), (int)(height * 0.15)));
+        bin.setIcon(new ImageIcon(garbageBin
+            .getScaledInstance((int)(bin.getWidth()-10), (int)(bin.getHeight()-5), 0)));
+        lowPanel.setBorder(new EmptyBorder((int)(height*0.2), (int)(width*0.2), 
+            (int)(height*0.09), (int)(width*0.12)));
     }
 
     public void start() {
