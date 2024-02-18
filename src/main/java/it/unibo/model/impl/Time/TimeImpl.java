e857318a2a3256a06f9b9b75ece952a1faff21b2
Andrea Nuvoli <andrea.nuvoli@studio.unibo.it>
2024-02-18 15:10:13 +0100

    Modified the interface of TimeImpl and made its JavaDoc

--- src/main/java/it/unibo/model/impl/Time/TimeImpl.java
+++ src/main/java/it/unibo/model/impl/Time/TimeImpl.java
@@ -3,9 +3,10 @@ package it.unibo.model.impl.Time;
 import java.util.*;
 import java.beans.*;
 
+import it.unibo.model.api.Time;
 import it.unibo.model.impl.Management.AbstractManager;
 
-public class TimeImpl {
+public class TimeImpl implements Time {
 
     final static int TIME_FOR_15_MINUTES = 15_000;
 
@@ -21,6 +22,7 @@ public class TimeImpl {
     private Timer timer;
     private PropertyChangeSupport support;
 
+    @Override
     public void incrementTime() {
         if(min == 45) {
             min = 0;
@@ -52,6 +54,7 @@ public class TimeImpl {
         }, 0, TIME_FOR_15_MINUTES);
     }
 
+    @Override
     public void newDay() {
         this.support = new PropertyChangeSupport(this);
         this.hour = STARTING_HOUR;
@@ -77,14 +80,15 @@ public class TimeImpl {
     }
 
     public String getHourAndMin() {
-        return new String(this.getHour() + " : " + this.getMin());
+        if(this.min == 0) {
+            return new String(this.getHour() + " : 00 ");
+        } else {
+            return new String(this.getHour() + " : " + this.getMin());
+        }
     }
 
-    public void addPropertyChangeListener(PropertyChangeListener pcl) {
-        support.addPropertyChangeListener(pcl);
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
     }
 
-    public void removePropertyChangeListener(PropertyChangeListener pcl) {
-        support.removePropertyChangeListener(pcl);
-    }
 }
