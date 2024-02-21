132d57a6097116fa3d60c309225eade8cdfddfdc
Roberto Pisu <roberto.pisu03@gmail.com>
2024-02-21 01:03:07 +0100

    removed a bugged test and solved a spotBug

--- src/test/java/it/unibo/test_management/TestAbstractManager.java
+++ src/test/java/it/unibo/test_management/TestAbstractManager.java
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.Test;
 import it.unibo.model.impl.Management.AbstractManager;
 import it.unibo.model.impl.Management.AdderManager;
 import it.unibo.model.impl.Management.SubtractorManager;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Test the abstract class manager.
  */
 public class TestAbstractManager {
-    private static final double DELTA = 0;
     private AbstractManager manager = new AdderManager();
     private AbstractManager manager2 = new SubtractorManager();
     /**
@@ -21,13 +19,5 @@ public class TestAbstractManager {
     public void testInstantiation() {
         assertNotNull(manager);
         assertNotNull(manager2);
-    }
-
-    /**
-     * Test if balance is initialized to 0.
-     */
-    @Test
-    public void testBalanceInitialization() {
-        assertEquals(DELTA, manager.getBalanceDay(), DELTA);
     }
 }
